41edfa1be36651c12cfbeaea778c1a1a9897749b
Victor Zagorodny <post.vittorius@gmail.com>
2015-04-09 00:16:08 +0300

    Add TODO

--- spec/rspec_fuzzy_time_spec.rb
+++ spec/rspec_fuzzy_time_spec.rb
@@ -5,7 +5,5 @@ describe RspecFuzzyTime do
     expect(RspecFuzzyTime::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  # TODO: test that adding configuration settings is working and they have assigned values 
 end
